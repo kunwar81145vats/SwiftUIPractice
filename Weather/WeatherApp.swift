f1e4756f6ba907016f3b6c543a546ca32c7dbf46
kunwar81145vats <97743194+kunwar81145vats@users.noreply.github.com>
2023-10-05 16:07:49 -0400

    Simple weather app ui

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Kunwar Vats on 03/10/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
